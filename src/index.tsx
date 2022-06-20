30dfa4c46bfeec19fc56494e577d34ac174d9151
Yuri Pashkevich <shmulhe@gmail.com>
2022-06-20 17:03:13 +0300

    add hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,10 @@
 import { render } from 'solid-js/web'
 import { App } from 'app'
 import { server } from 'shared/api'
-import { Router } from "solid-app-router"
+import { Router, hashIntegration } from "solid-app-router"
 
 
 server.start()
 server.log()
 
-render(() => <Router><App /></Router>, document.getElementById('root') as HTMLElement);
+render(() => <Router source={hashIntegration()}><App /></Router>, document.getElementById('root') as HTMLElement);
